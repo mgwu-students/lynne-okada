62dbc0ffe7c75e183f605ef658b039bdd4e112fb
lokada <lyokada@ucsc.edu>
2014-07-11 12:32:16 -0700

    retry button working

---
+++ Highscore.h
@@ -0,0 +1,13 @@
+//
+//  Highscore.h
+//  lynneokada
+//
+//  Created by Lynne Okada on 7/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCScene.h"
+
+@interface Highscore : CCScene
+
+@end
---
+++ Highscore.m
@@ -0,0 +1,13 @@
+//
+//  Highscore.m
+//  lynneokada
+//
+//  Created by Lynne Okada on 7/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Highscore.h"
+
+@implementation Highscore
+
+@end
