6c6ecee52e3c50e9430f9eba551de088853cd745
lokada <lyokada@ucsc.edu>
2014-07-13 12:22:22 -0700

    Astronaut enters without it being dragged into spritebuilder MainScene

---
+++ Astronaut.h
@@ -0,0 +1,13 @@
+//
+//  Astronaut.h
+//  lynneokada
+//
+//  Created by Lynne Okada on 7/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Astronaut : CCNode
+
+@end
---
+++ Astronaut.m
@@ -0,0 +1,13 @@
+//
+//  Astronaut.m
+//  lynneokada
+//
+//  Created by Lynne Okada on 7/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Astronaut.h"
+
+@implementation Astronaut
+
+@end
