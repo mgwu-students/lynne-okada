01dd93ec3d100bfc0609b8e37997e9c326824600
lokada <lyokada@ucsc.edu>
2014-07-14 22:40:31 -0700

    Stranded astronauts added

---
+++ StrandedAstronaut.h
@@ -0,0 +1,13 @@
+//
+//  StrandedAstronaut.h
+//  lynneokada
+//
+//  Created by Lynne Okada on 7/14/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface StrandedAstronaut : CCNode
+
+@end
---
+++ StrandedAstronaut.m
@@ -0,0 +1,13 @@
+//
+//  StrandedAstronaut.m
+//  lynneokada
+//
+//  Created by Lynne Okada on 7/14/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "StrandedAstronaut.h"
+
+@implementation StrandedAstronaut
+
+@end
