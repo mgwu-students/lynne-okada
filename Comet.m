71682fa23c9bcc1aaa0f43cf850973a7c204b8aa
lokada <lyokada@ucsc.edu>
2014-07-22 15:45:01 -0700

    point system: imcrements correctly when astronaut collides into ship with stranded

---
+++ Comet.m
@@ -0,0 +1,13 @@
+//
+//  OneSecAstroid.m
+//  lynneokada
+//
+//  Created by Lynne Okada on 7/22/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "OneSecAstroid.h"
+
+@implementation OneSecAstroid
+
+@end
