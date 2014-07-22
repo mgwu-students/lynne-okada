4811e448a752ae5cc46a33e36825cd6ef6ee2b53
lokada <lyokada@ucsc.edu>
2014-07-22 11:19:33 -0700

    shield spawns when screen touch

---
+++ Shield.m
@@ -0,0 +1,13 @@
+//
+//  Shield.m
+//  lynneokada
+//
+//  Created by Lynne Okada on 7/22/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Shield.h"
+
+@implementation Shield
+
+@end
