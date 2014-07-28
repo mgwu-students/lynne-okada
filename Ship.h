0765db5cba7e38ed5f29a6bbd743dd017ce5bd9c
lokada <lyokada@ucsc.edu>
2014-07-28 12:15:53 -0700

    ship moves on swipe

---
+++ Ship.h
@@ -0,0 +1,13 @@
+//
+//  Ship.h
+//  lynneokada
+//
+//  Created by Lynne Okada on 7/28/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Ship : CCNode
+
+@end
