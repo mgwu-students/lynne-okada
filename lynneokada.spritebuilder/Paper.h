aa9afe262c0790766215aab599f2f363dbccd26f
lokada <lyokada@ucsc.edu>
2014-08-12 13:57:38 -0700

    tutorial, page spawns

---
+++ lynneokada.spritebuilder/Paper.h
@@ -0,0 +1,13 @@
+//
+//  Paper.h
+//  lynneokada
+//
+//  Created by Lynne Okada on 8/12/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Paper : CCNode
+
+@end
